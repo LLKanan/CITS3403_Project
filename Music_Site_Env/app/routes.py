862c9f3defa8942a4be61035d5d7318afa94b1ed
LLKanan <54023991+LLKanan@users.noreply.github.com>
2020-05-18 20:30:39 +0800

    Redirect to admin_home if admin

--- Music_Site_Env/app/routes.py
+++ Music_Site_Env/app/routes.py
@@ -23,6 +23,8 @@ def login():
 			flash('Invalid username or password')
 			return redirect(url_for('login'))
 		login_user(user, remember=form.remember_me.data)
+		if current_user.get_id() == 0:
+			return redirect(url_for('admin_home'))
 		next_page = request.args.get('next')
 		if not next_page or url_parse(next_page).netloc != '':
 			next_page = url_for('index')
